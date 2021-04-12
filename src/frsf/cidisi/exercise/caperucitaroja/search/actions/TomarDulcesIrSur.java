ed3a44e34467e8338aaff673cf3972d0c50a0da0
franciscogiancarelli <franciscogiancarelli@gmail.com>
2021-04-12 16:35:37 -0300

    Implementé TomarDulcesIrSur

--- src/frsf/cidisi/exercise/caperucitaroja/search/actions/TomarDulcesIrSur.java
+++ src/frsf/cidisi/exercise/caperucitaroja/search/actions/TomarDulcesIrSur.java
@@ -8,58 +8,88 @@ import frsf.cidisi.faia.state.EnvironmentState;
 
 public class TomarDulcesIrSur extends SearchAction {
 
-    /**
-     * This method updates a tree node state when the search process is running.
-     * It does not updates the real world state.
-     */
-    @Override
-    public SearchBasedAgentState execute(SearchBasedAgentState s) {
-        EstadoCaperucitaRoja agState = (EstadoCaperucitaRoja) s;
-        
-        // TODO: Use this conditions
-        // PreConditions: (hayDulces && !estaLoboFeroz && !hayArboles)
-        // PostConditions: cantidadDulcesObtenidos= cantidadDulcesObtenidos +cantidadDulcesLineaVista; posicionCaperucitaRoja()
-        
-        return null;
-    }
+  /**
+   * This method updates a tree node state when the search process is running. It
+   * does not updates the real world state.
+   */
+  @Override
+  public SearchBasedAgentState execute(SearchBasedAgentState s) {
+    EstadoCaperucitaRoja agState = (EstadoCaperucitaRoja) s;
 
-    /**
-     * This method updates the agent state and the real world state.
-     */
-    @Override
-    public EnvironmentState execute(AgentState ast, EnvironmentState est) {
-        EstadoAmbiente environmentState = (EstadoAmbiente) est;
-        EstadoCaperucitaRoja agState = ((EstadoCaperucitaRoja) ast);
+    // TODO: Use this conditions
+    // PreConditions: (hayDulces && !estaLoboFeroz && !hayArboles)
+    // PostConditions: cantidadDulcesObtenidos= cantidadDulcesObtenidos
+    // +cantidadDulcesLineaVista; posicionCaperucitaRoja()
 
-        // TODO: Use this conditions
-        // PreConditions: (hayDulces && !estaLoboFeroz && !hayArboles)
-        // PostConditions: cantidadDulcesObtenidos= cantidadDulcesObtenidos +cantidadDulcesLineaVista; posicionCaperucitaRoja()
-        
-        if (true) {
-            // Update the real world
-            
-            // Update the agent state
-            
-            return environmentState;
+    return null;
+  }
+
+  /**
+   * This method updates the agent state and the real world state.
+   */
+  @Override
+  public EnvironmentState execute(AgentState ast, EnvironmentState est) {
+    EstadoAmbiente estadoAmbiente = (EstadoAmbiente) est;
+    EstadoCaperucitaRoja estadoCaperucitaRoja = ((EstadoCaperucitaRoja) ast);
+
+    int[] posicionCaperucitaRoja = estadoCaperucitaRoja.getposicionCaperucitaRoja();
+    int[] posicionLoboFeroz = estadoCaperucitaRoja.getposicionLoboFeroz();
+    int[][] mapa = estadoCaperucitaRoja.getmapa();
+
+    // Si no hay �rbol en la celda superior
+    if (mapa[posicionCaperucitaRoja[0]][posicionCaperucitaRoja[1] + 1] != 3) {
+      Boolean estaLoboFeroz = false;
+      Boolean hayDulces = false;
+      int j = posicionCaperucitaRoja[1] + 1;
+      while ((j < mapa[0].length) && (mapa[posicionCaperucitaRoja[0]][j] != 3)) {
+        if ((posicionCaperucitaRoja[0] == posicionLoboFeroz[0]) && (j == posicionLoboFeroz[1])) {
+          // Lobo Feroz se encuentra en la direcci�n norte
+          estaLoboFeroz = true;
         }
+        if (mapa[posicionCaperucitaRoja[0]][j] == 1) {
+          mapa[posicionCaperucitaRoja[0]][j] = 2;
+          int mapaAmbiente[][] = estadoAmbiente.getmapa();
+          mapaAmbiente[posicionCaperucitaRoja[0]][j] = 0;
+          estadoAmbiente.setmapa(mapaAmbiente);
+          estadoAmbiente.setcantidadDulcesMapa(estadoAmbiente.getcantidadDulcesMapa() - 1);
+          estadoCaperucitaRoja.setcantidadDulcesObtenidos(estadoCaperucitaRoja.getcantidadDulcesObtenidos() + 1);
+        }
+        j++;
+      }
+      // Si no est� el Lobo Feroz y no hay dulces en la direcci�n del norte
+      // entonces se puede avanzar al norte
+      if (!estaLoboFeroz && !hayDulces) {
+        // Caperucita Roja avanza hasta la celda vac�a anterior a �rbol
+        posicionCaperucitaRoja[1] = j;
+      }
 
-        return null;
+      if (!estaLoboFeroz && !hayDulces) {
+        // Update the real world
+        estadoAmbiente.setPosicionCaperucitaRoja(posicionCaperucitaRoja);
+
+        // Update the agent state
+        estadoCaperucitaRoja.setposicionCaperucitaRoja(posicionCaperucitaRoja);
+
+      }
     }
 
-    /**
-     * This method returns the action cost.
-     */
-    @Override
-    public Double getCost() {
-        return new Double(0);
-    }
+    return estadoAmbiente;
+  }
 
-    /**
-     * This method is not important for a search based agent, but is essensial
-     * when creating a calculus based one.
-     */
-    @Override
-    public String toString() {
-        return "TomarDulcesIrSur";
-    }
+  /**
+   * This method returns the action cost.
+   */
+  @Override
+  public Double getCost() {
+    return new Double(0);
+  }
+
+  /**
+   * This method is not important for a search based agent, but is essensial when
+   * creating a calculus based one.
+   */
+  @Override
+  public String toString() {
+    return "TomarDulcesIrSur";
+  }
 }
